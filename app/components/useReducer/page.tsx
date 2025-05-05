f64c6c7850ee8cdafc6fd1b3cbed0901fa1ddd14
omegamaster86 <masya868686@gmail.com>
2025-05-05 12:40:45 +0900

    update

--- app/components/useReducer/page.tsx
+++ app/components/useReducer/page.tsx
@@ -2,34 +2,28 @@
 
 import { useReducer } from 'react'
 
-type State = {
-  count: number;
-}
-
 type Action = {
   type: 'increment' | 'decrement';
 }
 
-const initialState: State = { count: 0 };
-
-const reducer = (state: State, action: Action): State => {
+const reducer = (state: number, action: Action): number => {
   switch (action.type) {
     case 'increment':
-      return { count: state.count + 1 };
+      return state + 1;
     case 'decrement':
-      return { count: state.count - 1 };
+      return state - 1;
     default:
       return state;
   }
 };
 
 export default function useReducerPage() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, 0);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <p>useReducer</p>
-      <p>{state.count}</p>
+      <p>{state}</p>
       <button type="button" onClick={() => dispatch({ type: 'increment' })}>+</button>
       <button type="button" onClick={() => dispatch({ type: 'decrement' })}>-</button>
     </div>
